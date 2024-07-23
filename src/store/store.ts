7c4eb49a30c2262df0e0e128de1dcca4a95c35c2
ericgro2000 <eric.gro.2000@gmail.com>
2024-07-24 00:43:52 +0200

    sheeeesh

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,22 +1,28 @@
 import createSagaMiddleware from "@redux-saga/core";
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import countReducer from "./countReducer";
 import userReducer from "./userReducer";
+import { rootWatcher } from "../saga/index";
 
 const rootReducer = combineReducers({
-    countReducer,
-    userReducer
-})
+  countReducer,
+  userReducer,
+});
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const setupStore = () => {
-    return configureStore({
-        reducer: rootReducer,
-        middleware: (getDefaultMiddleware) =>
-            getDefaultMiddleware()
-                .concat(createSagaMiddleware())
-    })
-}
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(sagaMiddleware),
+  });
 
-export type RootState = ReturnType<typeof rootReducer>
-export type AppStore = ReturnType<typeof setupStore>
-export type AppDispatch = AppStore['dispatch']
+  sagaMiddleware.run(rootWatcher);
+
+  return store;
+};
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
